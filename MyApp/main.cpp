b98d201deae419becc9709e409daf45b0eb7cabf
W1nd0w55 <iliader@outlook.com>
2025-02-24 14:41:49 +0500

    Moved entry point

--- MyApp/main.cpp
+++ MyApp/main.cpp
@@ -1,1 +1,10 @@
 #include <Core>
+
+int main(int argc, char** argv)
+{
+	MyApp::Core::Application* app = new MyApp::Core::Application;
+	int exitCode = app->Run();
+
+	delete app;
+	return exitCode;
+}
